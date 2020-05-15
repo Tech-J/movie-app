bbe77aff4153e4c6d098ae2ae0b64d0c916fe9c7
Tech-J <jchapman0819@gmail.com>
2020-05-15 02:00:17 -0400

    finish api routes -popular and one moive

--- server.js
+++ server.js
@@ -9,18 +9,42 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-app.get('/api/latest',(req, res)=>{
+//route to find individual movie
+app.get('/api/movies/:id',(req,res)=>{
+  let movieId = req.params.id || 297761;
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${tmDB}&language=en-US&append_to_response=credits`)
+  .then(data =>{
+    let movieObj = {
+      title: data.data.original_title,
+      description: data.data.overview,
+      genres: data.data.genres,
+      image: `https://image.tmdb.org/t/p/w500/${data.data.poster_path}`,
+      vote: data.data.vote_average,
+      vote_count: data.data.vote_count,
+      budget: data.data.budget.toLocaleString("en-US"),
+      revenue: data.data.revenue.toLocaleString("en-US"),
+      cast: data.data.credits.cast,
+      date: data.data.release_date,
+      time: data.data.runtime
+    }
+    res.status(200).json(movieObj)
+  })
+  .catch(err => console.log(err))
+})
+
+
+//route to find popular movies
+app.get('/api/movies',(req, res)=>{
   axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${tmDB}&language=en-US`)
   .then(response => {
-    let { results } = response.data
-    let responseArray = results.map(info=>{
+    let movieArray = response.data.results.map(info=>{
       return{
         title: info.original_title,
         poster: `https://image.tmdb.org/t/p/w500/${info.poster_path}`,
         vote: info.vote_average
       }
     })
-    res.status(200).send(responseArray)
+    res.status(200).json(movieArray)
   })
   .catch(err => res.send(err))
 })
