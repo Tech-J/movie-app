de29b008ac2d02e76a2df51800c16cc7c038ea8d
Tech-J <jchapman0819@gmail.com>
2020-05-14 23:14:38 -0400

    add route for latest movies

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-
+app.get('/api/latest',(req, res)=>{
+  axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${tmDB}&language=en-US`)
+  .then(response => {
+    let { results } = response.data
+    let responseArray = results.map(info=>{
+      return{
+        title: info.original_title,
+        poster: `https://image.tmdb.org/t/p/w500/${info.poster_path}`,
+        vote: info.vote_average
+      }
+    })
+    res.status(200).send(responseArray)
+  })
+  .catch(err => res.send(err))
+})
 
 app.listen(port,()=>console.log(`listening on ${port}`))
 
